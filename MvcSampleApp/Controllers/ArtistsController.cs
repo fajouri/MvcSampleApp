3286be1acbbfe77981b27cc16b6b1898217c030d
yfajouri <yef03@hotmail.com>
2015-06-03 17:58:03 -0300

    1st commit

---
+++ MvcSampleApp/Controllers/ArtistsController.cs
@@ -0,0 +1,82 @@
+﻿using MvcSampleApp.Core.Models;
+using MvcSampleApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSampleApp.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        // GET: Artists
+        public ActionResult Index()
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var artists = context.Artists.Distinct().ToList();                
+                return View(artists);
+            }            
+        }
+
+        public ActionResult New()
+        {
+            Artist artist = new Artist();
+            return View(artist);
+        }
+
+        [HttpPost]
+        public ActionResult New(Artist artist)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var artists = context.Artists.Add(artist);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int artistId)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var artist = context.Artists.FirstOrDefault(a => a.ArtistId.Equals(artistId));
+                return View(artist);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(Artist artist)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var currentArtist = context.Artists.FirstOrDefault(a => a.ArtistId.Equals(artist.ArtistId));
+                currentArtist.Name = artist.Name;
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int artistId)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var artist = context.Artists.FirstOrDefault(a => a.ArtistId.Equals(artistId));
+                return View(artist);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteArtist(int artistId)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var artist = context.Artists.FirstOrDefault(a => a.ArtistId.Equals(artistId));
+                context.Artists.Remove(artist);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcSampleApp/Controllers/GenresController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSampleApp.Controllers
+{
+    using Core.Models;
+    using Data;
+
+    public class GenresController : Controller
+    {
+        //
+        // GET: /Genres/
+        public ActionResult Index()
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var genres = context.Genres.ToList();
+                return View(genres);
+            }
+        }
+
+        public ActionResult New()
+        {
+            var genre = new Genre();
+
+            return View(genre);
+        }
+
+        [HttpPost]
+        public ActionResult New(Genre genre)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                context.Genres.Add(genre);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int genreId)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var genre = context.Genres.FirstOrDefault(g => g.GenreId.Equals(genreId));
+
+                return View(genre);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(Genre genre)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var currentGenre = context.Genres.FirstOrDefault(g => g.GenreId.Equals(genre.GenreId));
+                currentGenre.Name = genre.Name;
+                currentGenre.Description = genre.Description;
+
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int genreId)
+        {
+            using (var context = new MusicStoreContext())
+            {
+                var genre = context.Genres.FirstOrDefault(g => g.GenreId.Equals(genreId));
+
+                return View(genre);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteGenre(int genreId)
+        {
+            using ( var context = new MusicStoreContext())
+            {
+                var genre = context.Genres.FirstOrDefault(g => g.GenreId.Equals(genreId));
+                context.Genres.Remove(genre);
+
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+	}
+}
